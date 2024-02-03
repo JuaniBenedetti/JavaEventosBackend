bf945818076a45c60938f243ff1d7a65b05ac873
JuaniBenedetti <101596119+JuaniBenedetti@users.noreply.github.com>
2024-02-03 19:51:24 -0300

    Modificación a SalonController para que tome automáticamente el administrativo asociado al usuario que lo crea

---
+++ src/main/java/eventos/eventos/dao/administrativo/AdministrativoDao.java
@@ -0,0 +1,10 @@
+package eventos.eventos.dao.administrativo;
+
+import eventos.eventos.dao._CRUD.CRUDDao;
+import eventos.eventos.model.Administrativo;
+import eventos.eventos.model.Usuario;
+
+public interface AdministrativoDao extends CRUDDao<Administrativo> {
+
+    Administrativo findByUsuario(Usuario usuario) throws Exception;
+}
--- src/main/java/eventos/eventos/services/cliente/ClienteServiceImpl.java
+++ src/main/java/eventos/eventos/services/cliente/ClienteServiceImpl.java
@@ -4,11 +4,7 @@ import eventos.eventos.dao.cliente.ClienteDao;
 import eventos.eventos.model.Cliente;
 import eventos.eventos.model.Usuario;
 import eventos.eventos.services._CRUD.CRUDServiceImpl;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class ClienteServiceImpl extends CRUDServiceImpl<Cliente> implements ClienteService{
