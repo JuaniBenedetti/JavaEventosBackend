01fd8b792a90a0c93fef43bc40699fa1ada3e08c
JuaniBenedetti <101596119+JuaniBenedetti@users.noreply.github.com>
2024-08-02 20:34:26 -0300

    Prueba deploy en Render
    Se vencio el plazo de prueba para Railway

--- src/main/java/eventos/eventos/services/sendgrid/SendGridServiceImpl.java
+++ src/main/java/eventos/eventos/services/sendgrid/SendGridServiceImpl.java
@@ -28,7 +28,7 @@ public class SendGridServiceImpl implements SendGridService{
 
         Personalization personalization = new Personalization();
         personalization.addSubstitution("{{code}}", code);
-        personalization.addSubstitution("{{{unsubscribe}}}", "https://trollface.dk/");
+        personalization.addSubstitution("{{{unsubscribe}}}", "https://eventosjava.web.app");
         personalization.addTo(to);
 
         Mail mail = new Mail();
